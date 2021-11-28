dabd895a7ec6d4fd613e2de54a1b3c0cc434c848
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-11-28 11:06:31 +0800

    laptop sidebar file creation

---
+++ src/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            this is a side bar
+        </div>
+    )
+}
+
+export default Sidebar
