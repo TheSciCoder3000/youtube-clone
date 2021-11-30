d0d26c35db5c75cf666a049434a7c8809e7f3199
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-11-30 22:37:32 +0800

    react router dom update

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,33 @@ import './App.css';
 import Header from './Header'
 import Sidebar from './Components/Sidebar/Sidebar'
 import Recommended from './Components/Recommended/Recommended'
+import Search from './Components/Search/Search'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__viewer">
-        <Sidebar />
-        <Recommended />
-      </div>
+      <Router>
+        <Header />
+        <div className="app__viewer">
+          <Sidebar />
+
+          <Switch>
+            <Route exact path="/search">
+              <div className="app__no-results">
+                <h3>There are no Results</h3>
+                <p>please search for something else</p>
+              </div>
+            </Route>
+            <Route path={`/search/:searchData`}>
+              <Search />
+            </Route>
+            <Route exact path="/">
+              <Recommended />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
