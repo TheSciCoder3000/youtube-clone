967f1b277daed2529cd9be0310ae2dadc26ca1bc
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-12-01 21:30:55 +0800

    removing firebase keys and data

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDYGBaoAN_T7m1kGUdUBPZ0q7J99N_uMPI",
-  authDomain: "clone-bdbec.firebaseapp.com",
-  projectId: "clone-bdbec",
-  storageBucket: "clone-bdbec.appspot.com",
-  messagingSenderId: "479705082608",
-  appId: "1:479705082608:web:a8987d1c1d44d24579f035",
-  measurementId: "G-Z97DN3R9FC"
-};
